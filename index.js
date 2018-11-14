dd2249b46fce7429d722519b373a222eac7efc53
copatachia <copatachia@pentalog.com>
2018-11-14 18:56:28 +0200

    webpack prepare modified babel settings added index.html installed html dependency modified webpack config file

---
+++ index.js
@@ -0,0 +1,8 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+    return <Fragment>Hello React</Fragment>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('index'));
