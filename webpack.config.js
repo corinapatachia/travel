eb3f880c080bfd34a2f0bbbc3f8aa1e6154d4357
copatachia <copatachia@pentalog.com>
2018-11-14 18:43:41 +0200

    add webpack config for v4 add dist folder to gitignore file move some dependencies to dev-dependencies in package.json added console.log in index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,13 @@
 module.exports = {
-  entry: ['./src/index.js'],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
         }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './',
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
+      ]
     }
-  }
-};
-
+  };
