468094a3d8d30068cfc35ba0d150293f09dbfa90
copatachia <copatachia@pentalog.com>
2018-11-16 16:40:49 +0200

    print helloworld in index.js and added open adn hot to the start script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('jello');
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+    return <Fragment>Hello World</Fragment>;
+}
+
+ReactDOM.render(<Index/>, document.getElementById("index"));
